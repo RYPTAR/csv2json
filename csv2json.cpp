4bfededc872a72f408aee5567f1dec6d57ab0ea5
RYPTAR <ray.dej21@gmail.com>
2018-03-14 17:38:51 -0400

    Comments

--- csv2json.cpp
+++ csv2json.cpp
@@ -18,25 +18,35 @@ class elements{
 }*/
 
 int main(int argc, char* const argv[]){
+
+    // Pulls in file
     ifstream file(argv[1]);
     string line;
+
     //TODO: multiple arguements
 
+    // Decodes command
     string args = string(argv[2]);
     string command = args.substr(0,args.find('['));
     //cout<<command<<endl;
 
+
     if(command == "struct"){
+
+        // Finds list of command arguements
         size_t length = args.find(']') - args.find('[');
         string subArgs = args.substr(args.find('[') + 1, length - 1);
         //cout<<subArgs<<endl;
-
+        
+        // Locates commas
         vector<size_t> comma;
         for( int i = 0; i < subArgs.length(); ++i){
             if(subArgs[i] == ',')
                 comma.push_back(i);
         }
         //cout<<comma.size()<<endl;
+
+        // Decodes the command arguements
         vector<string> order;
         for( int i = 0; i < comma.size(); ++i){
             if(i == 0)
