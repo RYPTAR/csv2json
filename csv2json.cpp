767c6088af4be3c99170655a8f4ef9abc1e3693e
RYPTAR <ray.dej21@gmail.com>
2018-03-14 17:34:46 -0400

    Fix substr operations, decodes arguments

--- csv2json.cpp
+++ csv2json.cpp
@@ -24,22 +24,33 @@ int main(int argc, char* const argv[]){
 
     string args = string(argv[2]);
     string command = args.substr(0,args.find('['));
+    //cout<<command<<endl;
 
     if(command == "struct"){
-        string subArgs = args.substr(args.find('[') + 1);
-        subArgs.pop_back();
-        //cout<<command<<endl;
-        //cout<<order<<endl;
+        size_t length = args.find(']') - args.find('[');
+        string subArgs = args.substr(args.find('[') + 1, length - 1);
+        //cout<<subArgs<<endl;
 
-        vector<int> comma;
+        vector<size_t> comma;
         for( int i = 0; i < subArgs.length(); ++i){
             if(subArgs[i] == ',')
                 comma.push_back(i);
         }
-        cout<<comma.size()<<endl;
-        /*vector<string> order;
-        for( int i = 0; i < comma.size() + 2){
-            order
+        //cout<<comma.size()<<endl;
+        vector<string> order;
+        for( int i = 0; i < comma.size(); ++i){
+            if(i == 0)
+                order.push_back(subArgs.substr(0, comma[i]));
+
+            if( i == comma.size() )
+                order.push_back(subArgs.substr(comma[i-1] + 1));
+            else{
+                size_t length = comma[i+1] - comma[i];
+                order.push_back(subArgs.substr(comma[i] + 1, length - 1));   
+            }
+        }
+        /*for( int i = 0; i < order.size(); ++i){
+            cout<<order[i]<<endl;
         }*/
 
         //while(getline(file, line)){
